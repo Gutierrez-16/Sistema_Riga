1a03e52c443008508b98d6ce66610c09843d1043
Alexis Gutierrez <jospers557@gmail.com>
2024-04-11 19:30:24 -0500

    add:api login

--- sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/SistemaRigaBackendApplication.java
+++ sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/SistemaRigaBackendApplication.java
@@ -1,6 +1,7 @@
 package com.sistema.riga.sistema_riga_backend;
 
 import com.sistema.riga.sistema_riga_backend.models.DistritoModel;
+import com.sistema.riga.sistema_riga_backend.models.LoginRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,9 @@ public class SistemaRigaBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaRigaBackendApplication.class, args);
+		LoginRequest loginRequest = new LoginRequest();
+		loginRequest.setUsername("ale");
+		loginRequest.setPassword("ale");
+
 	}
 }
