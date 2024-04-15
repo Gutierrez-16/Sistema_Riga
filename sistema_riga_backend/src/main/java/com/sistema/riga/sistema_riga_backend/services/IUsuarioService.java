4ad62f85d95a48cec5f1667962af27bbe118a007
Rigebs <riclarobr04@gmail.com>
2024-04-14 21:36:10 -0500

    add: Login mejorado

---
+++ sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/services/IUsuarioService.java
@@ -0,0 +1,5 @@
+package com.sistema.riga.sistema_riga_backend.services;
+
+public interface IUsuarioService {
+    boolean isValidUser(String username, String password);
+}
--- sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/services/LoginService.java
+++
@@ -1,2 +0,0 @@
-package com.sistema.riga.sistema_riga_backend.services;public class LoginService {
-}
