59edf1d25511020db1e5e9773a38e23c56273328
Alexis Gutierrez <jospers557@gmail.com>
2024-05-18 20:44:00 -0500

    login

--- sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/configuration/PasswordEncoderGenerator.java
+++ sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/configuration/PasswordEncoderGenerator.java
@@ -4,7 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoderGenerator {
     public static void main(String[] args) {
-        String password = "clave2";
+        String password = "chamo";
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode(password);
 
