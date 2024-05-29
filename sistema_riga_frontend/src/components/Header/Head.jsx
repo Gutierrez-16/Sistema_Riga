f7b72949a0f19348b591f38a5e8b670f3c64a598
Alexis Gutierrez <jospers557@gmail.com>
2024-05-29 16:22:28 -0500

    add file

--- sistema_riga_frontend/src/components/Header/Head.jsx
+++ sistema_riga_frontend/src/components/Header/Head.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
-import './HeadStyle.css';
-import { FiShoppingCart } from "react-icons/fi";
-import { FaCircleUser } from "react-icons/fa6";
+import React, { useState } from 'react';
+import { Menu } from 'primereact/menu';
+import { Avatar } from 'primereact/avatar';
+import { Button } from 'primereact/button';
 
+export default function HeaderMenu() {
+    const [menuOpen, setMenuOpen] = useState(false);
 
-const Head = () => {
-    return (
-        <header>
-            <div className='head'>
-                <h1>EMPRESA RIGA</h1>
-            </div>
-        </header>
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const itemRenderer = (item) => (
+        <div className='p-menuitem-content'>
+            <a className="flex align-items-center p-menuitem-link">
+                <span className={item.icon} />
+                <span className="mx-2">{item.label}</span>
+                {item.badge && <span className="ml-auto">{item.badge}</span>}
+                {item.shortcut && <span className="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{item.shortcut}</span>}
+            </a>
+        </div>
     );
-};
 
-export default Head;
+    let items = [
+        // Items configuration...
+    ];
+
+    return (
+        <div>
+            <Button icon="pi pi-bars" onClick={toggleMenu} />
+            <Menu model={items} popup={true} onHide={() => setMenuOpen(false)} visible={menuOpen} />
+        </div>
+    );
+}
