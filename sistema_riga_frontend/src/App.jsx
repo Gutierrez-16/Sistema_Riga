c50b44895db401b6039ac68fdc086675295419fb
Shujaa <153398359+Gutierrez-16@users.noreply.github.com>
2024-06-15 00:49:35 -0500

    Update App.jsx

--- sistema_riga_frontend/src/App.jsx
+++ sistema_riga_frontend/src/App.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./Components/Login/ProtectedRoute";
 import LoginApp from "./Components/Login/LoginApp";
 import Usuario from "./Components/Usuario/Usuario";
-import VentasTotal from "./Components/Ventas/ListarVentas";
-import Reporte from "./Components/Reporte/Reporte";
 import Persona from "./Components/Person/Persona";
 import Producto from "./Components/Product/Producto";
-import UnidadMedidad from "./Components/UnidadMedida/UnidadMedida";
-import TipoUsuario from "./Components/TipoUsuario/TipoUsuario";
-import Marca from "./Components/Marca/Marca";
-import Caja from "./Components/Caja/Caja";
 import Empleado from "./Components/Employer/Empleado";
 import Empresa from "./Components/Empresa/Empresa";
 import Home from "./Components/Home/Home";
@@ -18,33 +11,44 @@ import Cargo from "./Components/Cargo/Cargo";
 import Linea from "./Components/Linea/Linea";
 import Categoria from "./Components/Categoria/Categoria";
 import MetodoPago from "./Components/MetodoPago/MetodoPago";
+import UnidadMedidad from "./Components/UnidadMedida/UnidadMedida";
+import TipoUsuario from "./Components/TipoUsuario/TipoUsuario";
+import Marca from "./Components/Marca/Marca";
+import Caja from "./Components/Caja/Caja";
+import Reporte from "./Components/Reporte/Reporte";
+import VentasTotal from "./Components/Ventas/ListarVentas";
 import "./App.css";
 
 function App() {
   return (
     <div className="pruebas">
       <Routes>
-        <Route path="/login" element={<LoginApp />} />
-
-        {/* Rutas protegidas */}
-        <Route path="/" element={<ProtectedRoute><Usuario /></ProtectedRoute>} />
-        <Route path="/usuario" element={<ProtectedRoute><Usuario /></ProtectedRoute>} />
-        <Route path="/listaventas" element={<ProtectedRoute><VentasTotal /></ProtectedRoute>} />
-        <Route path="/reporte" element={<ProtectedRoute><Reporte /></ProtectedRoute>} />
-        <Route path="/logout" element={<ProtectedRoute><Usuario /></ProtectedRoute>} />
+        <Route
+          path="/login"
+          element={
+            <div className="hola">
+              <LoginApp />
+            </div>
+          }
+        />
+        <Route path="/" element={<Usuario />} />
+        <Route path="/usuario" element={<Usuario />} />
+        <Route path="/listaventas" element={<VentasTotal />} />
+        <Route path="/reporte" element={<Reporte />} />
+        <Route path="/logout" element={<Usuario />} />
         <Route path="/persona" element={<Persona />} />
-        <Route path="/producto" element={<ProtectedRoute><Producto /></ProtectedRoute>} />
-        <Route path="/unidadmedida" element={<ProtectedRoute><UnidadMedidad /></ProtectedRoute>} />
-        <Route path="/tipousuario" element={<ProtectedRoute><TipoUsuario /></ProtectedRoute>} />
-        <Route path="/marca" element={<ProtectedRoute><Marca /></ProtectedRoute>} />
-        <Route path="/caja" element={<ProtectedRoute><Caja /></ProtectedRoute>} />
-        <Route path="/empleado" element={<ProtectedRoute><Empleado /></ProtectedRoute>} />
-        <Route path="/empresa" element={<ProtectedRoute><Empresa /></ProtectedRoute>} />
-        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/cargo" element={<ProtectedRoute><Cargo /></ProtectedRoute>} />
-        <Route path="/linea" element={<ProtectedRoute><Linea /></ProtectedRoute>} />
-        <Route path="/categoria" element={<ProtectedRoute><Categoria /></ProtectedRoute>} />
-        <Route path="/metodopago" element={<ProtectedRoute><MetodoPago /></ProtectedRoute>} />
+        <Route path="/producto" element={<Producto />} />
+        <Route path="/unidadmedida" element={<UnidadMedidad />} />
+        <Route path="/tipousuario" element={<TipoUsuario />} />
+        <Route path="/marca" element={<Marca />} />
+        <Route path="/caja" element={<Caja />} />
+        <Route path="/empleado" element={<Empleado />} />
+        <Route path="/empresa" element={<Empresa />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cargo" element={<Cargo />} />
+        <Route path="/linea" element={<Linea />} />
+        <Route path="/categoria" element={<Categoria />} />
+        <Route path="/metodopago" element={<MetodoPago />} />
       </Routes>
     </div>
   );
