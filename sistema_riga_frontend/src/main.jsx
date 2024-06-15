b4bcd373b8e42518eccd148c057e544a3f7a6ff3
Rigebs <165223164+Rigebs@users.noreply.github.com>
2024-06-15 11:07:10 -0500

    fixed

--- sistema_riga_frontend/src/main.jsx
+++ sistema_riga_frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App'; // Import the main App component
+import App from './App';
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import './index.css';
