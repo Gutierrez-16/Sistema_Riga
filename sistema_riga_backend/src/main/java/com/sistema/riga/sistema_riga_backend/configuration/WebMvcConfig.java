f6d4fce3dc569450f76f5b6c2596b2573311cb77
Alexis Gutierrez <jospers557@gmail.com>
2024-06-14 22:35:24 -0500

    add: proyecto tern¿minado

--- sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/configuration/WebMvcConfig.java
+++ sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/configuration/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.sistema.riga.sistema_riga_backend.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+        registry.addMapping("/*")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("*")
+                .allowedHeaders("")
                 .allowCredentials(true);
     }
 }
