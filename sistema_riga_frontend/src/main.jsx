16eaa917773d425e41541635009759aa3f90ed6e
Rigebs <165223164+Rigebs@users.noreply.github.com>
2024-06-15 10:17:33 -0500

    fixed

--- sistema_riga_frontend/src/main.jsx
+++ sistema_riga_frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App'; // Import the main App component
+import App from './App';
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
