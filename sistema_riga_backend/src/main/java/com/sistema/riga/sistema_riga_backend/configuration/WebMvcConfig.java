d62893d8c7193000d8eafa7ab63ec8f4884fdd8e
Shujaa <153398359+Gutierrez-16@users.noreply.github.com>
2024-06-15 20:05:28 -0500

    Update WebMvcConfig.java

--- sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/configuration/WebMvcConfig.java
+++ sistema_riga_backend/src/main/java/com/sistema/riga/sistema_riga_backend/configuration/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://sistema-riga-app.netlify.app", "http://localhost:5173")
+                .allowedOrigins("https://sistema-riga-app.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
